6e41cbf4df5b6d0f789d32b87cfd2aa8b91a1850
소녀혜찐 <isk00194@gmail.com>
2019-04-16 14:38:52 +0900

    MainActivity 수정

--- RookiePhotoApp/app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
+++ RookiePhotoApp/app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
@@ -1,7 +1,12 @@
 package kr.co.yogiyo.rookiephotoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.camerakit.CameraKitView;
+
+import kr.co.yogiyo.rookiephotoapp.camera.CameraActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, CameraActivity.class));
     }
 }
