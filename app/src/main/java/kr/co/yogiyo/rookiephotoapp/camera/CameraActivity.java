85951d07e49631e69852216401e1b8670abfa77e
박병용(Byungyong, Park) <qkrquddyd94@gmail.com>
2019-04-17 16:47:46 +0900

    TOY-11 카메라 타이머 기능 개발

--- app/src/main/java/kr/co/yogiyo/rookiephotoapp/camera/CameraActivity.java
+++ app/src/main/java/kr/co/yogiyo/rookiephotoapp/camera/CameraActivity.java
@@ -2,13 +2,18 @@ package kr.co.yogiyo.rookiephotoapp.camera;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.camerakit.CameraKit;
 import com.camerakit.CameraKitView;
+
+import java.util.Arrays;
+import java.util.List;
 
 import kr.co.yogiyo.rookiephotoapp.R;
 import kr.co.yogiyo.rookiephotoapp.camera.capture.PreviewActivity;
@@ -16,14 +21,24 @@ import kr.co.yogiyo.rookiephotoapp.camera.capture.ResultHolder;
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final List<Integer> DELAY_DURATIONS = Arrays.asList(0, 2, 5, 10);
+    private static final int DEFAULT_DELAY = 0;
+
+    private Handler timerHandler;
+
+    private int captureTimer;
+    private int currentCaptureID;
+    private int captureDelay;
+
     private CameraKitView cameraKitView;
+    private FrameLayout controlButtonsFrameLayout;
     private Button backButton;
     private Button flashButton;
     private Button timerButton;
     private Button captureButton;
     private Button changeCameraButton;
     private TextView wariningTextView;
-
+    private TextView timerMessageTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,16 +46,20 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_camera);
 
         initView();
+
+        initialize();
     }
 
     private void initView() {
         cameraKitView = findViewById(R.id.camera);
+        controlButtonsFrameLayout = findViewById(R.id.frame_control_buttons);
         backButton = findViewById(R.id.btn_back);
         flashButton = findViewById(R.id.btn_flash);
         timerButton = findViewById(R.id.btn_timer);
         captureButton = findViewById(R.id.btn_capture);
         changeCameraButton = findViewById(R.id.btn_change_camera);
         wariningTextView = findViewById(R.id.tv_warning);
+        timerMessageTextView = findViewById(R.id.text_timer_message);
 
         cameraKitView.setOnClickListener(this);
         backButton.setOnClickListener(this);
@@ -48,6 +67,15 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         timerButton.setOnClickListener(this);
         captureButton.setOnClickListener(this);
         changeCameraButton.setOnClickListener(this);
+
+        updateDelayButton();
+    }
+
+    private void initialize() {
+        timerHandler = new Handler();
+        captureTimer = 0;
+        currentCaptureID = 0;
+        captureDelay = DEFAULT_DELAY;
     }
 
     @Override
@@ -64,17 +92,10 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
                 }
                 break;
             case R.id.btn_timer:
+                setCaptureDelayNext();
                 break;
             case R.id.btn_capture:
-                cameraKitView.captureImage(new CameraKitView.ImageCallback() {
-                    @Override
-                    public void onImage(CameraKitView cameraKitView, byte[] bytes) {
-                        ResultHolder.dispose();
-                        ResultHolder.setImage(bytes);
-                        Intent intent = new Intent(CameraActivity.this, PreviewActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                capture();
                 break;
             case R.id.btn_change_camera:
                 if (cameraKitView.getFacing() == CameraKit.FACING_FRONT) {
@@ -89,7 +110,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        finish();
+        if (timerHandler != null) {
+            finishDelayCapture();
+        } else {
+            finish();
+        }
     }
 
     @Override
@@ -102,11 +127,15 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     protected void onResume() {
         super.onResume();
         cameraKitView.onResume();
+        setControlButtonsVisibility(true);
     }
 
     @Override
     protected void onPause() {
         cameraKitView.onPause();
+        if (timerHandler != null) {
+            finishDelayCapture();
+        }
         super.onPause();
     }
 
@@ -122,5 +151,89 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
+    private void captureNow() {
+        cameraKitView.captureImage(new CameraKitView.ImageCallback() {
+            @Override
+            public void onImage(CameraKitView cameraKitView, byte[] bytes) {
+                ResultHolder.dispose();
+                ResultHolder.setImage(bytes);
+                Intent intent = new Intent(CameraActivity.this, PreviewActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void capture() {
+        if (captureDelay == DEFAULT_DELAY) {
+            captureNow();
+        } else {
+            captureAfterDelay(captureDelay);
+        }
+    }
+
+    private void setCaptureDelayNext() {
+        int index = DELAY_DURATIONS.indexOf(captureDelay);
+        if (index < 0) {
+            captureDelay = DEFAULT_DELAY;
+        } else {
+            captureDelay = DELAY_DURATIONS.get((index + 1) % DELAY_DURATIONS.size());
+        }
+        updateDelayButton();
+    }
+
+    private void updateDelayButton() {
+        if (captureDelay==0) {
+            timerButton.setText(getString(R.string.text_timer));
+        }
+        else {
+            String timerButtonTextFormat = getString(R.string.text_timer_button_text_format);
+            timerButton.setText(String.format(timerButtonTextFormat, captureDelay));
+        }
+    }
+
+    private void captureAfterDelay(int delay) {
+        timerMessageTextView.setVisibility(View.VISIBLE);
+        captureTimer = delay;
+        updateTimerMessage();
+        currentCaptureID++;
+        timerHandler.postDelayed(makeDecrementTimerFunction(currentCaptureID), 1000);
+
+        setControlButtonsVisibility(false);
+    }
+
+    private Runnable makeDecrementTimerFunction(final int captureID) {
+        return new Runnable() {
+            public void run() {
+                decrementTimer(captureID);
+            }
+        };
+    }
+
+    private void decrementTimer(final int captureID) {
+        if (captureID != currentCaptureID) {
+            return;
+        }
+        --captureTimer;
+        if (captureTimer == 0) {
+            captureNow();
+        } else if (captureTimer > 0) {
+            updateTimerMessage();
+            timerHandler.postDelayed(makeDecrementTimerFunction(captureID), 1000);
+        }
+    }
+
+    private void updateTimerMessage() {
+        String timerMessageFormat = getString(R.string.text_timer_message_format);
+        timerMessageTextView.setText(String.format(timerMessageFormat, captureTimer));
+    }
+
+    private void setControlButtonsVisibility(boolean allowSave) {
+        controlButtonsFrameLayout.setVisibility(allowSave ? View.VISIBLE : View.GONE);
+    }
+
+    private void finishDelayCapture() {
+        timerHandler.removeMessages(0);
+        timerMessageTextView.setVisibility(View.GONE);
+    }
 
 }
