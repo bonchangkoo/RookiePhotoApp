8de402e8a84e581398a8b63d1a72cce7a4a271fc
박병용(Byungyong, Park) <qkrquddyd94@gmail.com>
2019-05-01 19:42:59 +0900

    TOY-19 접근 권한 요청 라이브러리 적용, 앱 시작시 접근 권한 요청 기능 구현

--- app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
+++ app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
@@ -1,10 +1,15 @@
 package kr.co.yogiyo.rookiephotoapp;
 
+import android.Manifest;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import com.camerakit.CameraKitView;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.ArrayList;
 
 import kr.co.yogiyo.rookiephotoapp.camera.CameraActivity;
 
@@ -15,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, CameraActivity.class));
+        PermissionListener permissionlistener = new PermissionListener() {
+            @Override
+            public void onPermissionGranted() {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+
+            @Override
+            public void onPermissionDenied(ArrayList<String> deniedPermissions) {
+                finish();
+            }
+        };
+
+        TedPermission.with(this)
+                .setPermissionListener(permissionlistener)
+                .setDeniedMessage("단말기의 [설정] > [권한]에서 접근 권한을 ON으로 설정해주세요.\n\n* 카메라\n* 저장공간")
+                .setGotoSettingButtonText("설정")
+                .setDeniedCloseButtonText("취소")
+                .setPermissions(Manifest.permission.CAMERA,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                .check();
     }
 }
