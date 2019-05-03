76c66f1ca87c8343140509b990407a42330196ab
bonchangkoo <bonchang.koo@deliveryhero.co.kr>
2019-05-03 16:57:37 +0900

    TOY-15 CameraActivity에 MVVM 패턴 적용

--- app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
+++ app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
@@ -1,11 +1,10 @@
 package kr.co.yogiyo.rookiephotoapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-
-import kr.co.yogiyo.rookiephotoapp.camera.CameraActivity;
+import kr.co.yogiyo.rookiephotoapp.camera.CameraV2Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, CameraActivity.class));
+        startActivity(new Intent(this, CameraV2Activity.class));
     }
 }
