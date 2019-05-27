761e3fa92eadaf0c22e9816e9ff3d05a6ed0d0a0
박병용(Byungyong, Park) <qkrquddyd94@gmail.com>
2019-05-27 13:41:50 +0900

    TOY-36 pr 코드 수정

--- app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
+++ app/src/main/java/kr/co/yogiyo/rookiephotoapp/MainActivity.java
@@ -4,13 +4,9 @@ import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import com.facebook.stetho.Stetho;
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
 
@@ -18,16 +14,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import kr.co.yogiyo.rookiephotoapp.camera.CameraActivity;
-import kr.co.yogiyo.rookiephotoapp.edit.EditPhotoActivity;
 import kr.co.yogiyo.rookiephotoapp.gallery.GalleryActivity;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
-    // 갤러리에서 사진 선택할 때 같이 전달할 request code (test)
-    public static final int SELECT_GALLERY_PHOTO = 11111;
-
-    // 편집 화면으로 가기 위한 임시 버튼
-    private Button editPhotoButton;
     private Button pickFromGalleryButton;
 
     @Override
@@ -38,30 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         initView();
 
         setupCheckPermission();
-
-        // stetho 초기화 : chrome://inspect/ 에 접속해서 target 기기의 'inspect'를 누르면
-        // 기기에 대한 네트워크, db, sharedPreference 등의 정보를 확인할 수 있음
-        Stetho.initializeWithDefaults(this);
-    }
-
-    // Test code. 선택한 이미지의 uri를 받아 출력함
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_GALLERY_PHOTO) {
-                if (data != null) {
-                    if (data.getData() != null) {
-                        Toast.makeText(this, String.format("선택(하고 편집)한 이미지 uri : %s", data.getData().toString()), Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        }
     }
 
     private void initView() {
-        editPhotoButton = findViewById(R.id.btn_edit_photo);
-        editPhotoButton.setOnClickListener(this);
         pickFromGalleryButton = findViewById(R.id.btn_pick_from_gallery);
         pickFromGalleryButton.setOnClickListener(this);
     }
@@ -69,21 +38,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn_edit_photo:
-                // 사진 선택
-                Intent doStartEditPhotoActivityIntent = new Intent(this, EditPhotoActivity.class);
-                doStartEditPhotoActivityIntent.putExtra(getString(R.string.edit_photo_category_number), EditPhotoActivity.EDIT_SELECTED_PHOTO);
-                startActivity(doStartEditPhotoActivityIntent);
-                break;
             case R.id.btn_pick_from_gallery:
-                // 커스텀 갤러리의 사진 선택
-                // Test code. 선택한 이미지의 uri를 받아 출력함
-                startActivityForResult(new Intent(this, GalleryActivity.class), SELECT_GALLERY_PHOTO);
+                startActivity(new Intent(this, GalleryActivity.class));
                 break;
         }
     }
 
-    // Permission check
     private void setupCheckPermission() {
         PermissionListener permissionlistener = new PermissionListener() {
             @Override
@@ -97,7 +57,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         };
 
-        // 기기 버전별 요청 권한 추가
         List<String> permissions = new ArrayList<>();
         permissions.add(Manifest.permission.CAMERA);
         permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
@@ -105,7 +64,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         }
 
-        // 권한 확인 및 요청 라이브러리
         TedPermission.with(this)
                 .setPermissionListener(permissionlistener)
                 .setDeniedMessage("단말기의 [설정] > [권한]에서 접근 권한을 ON으로 설정해주세요.\n\n* 카메라\n* 저장공간")
--- app/src/main/java/kr/co/yogiyo/rookiephotoapp/gallery/GalleryAdapter.java
+++ app/src/main/java/kr/co/yogiyo/rookiephotoapp/gallery/GalleryAdapter.java
@@ -1,8 +1,8 @@
 package kr.co.yogiyo.rookiephotoapp.gallery;
 
+import android.content.Context;
 import android.net.Uri;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,42 +10,23 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
-import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.Glide;
 
 import java.io.File;
 import java.util.List;
 
 import kr.co.yogiyo.rookiephotoapp.R;
 
-/**
- * GalleryAdapter
- *   : GalleryFragment의 RecyclerView에 GridLayout(3) 방식으로 이미지를 보여줌
- *
- * GalleryAdapter 역할
- *   : ViewHolder 생성
- *   : 선택한 이미지의 ViewHolder, imagePath 임시 저장
- *   : 사용자가 선택한 폴더명의 이미지 파일 경로 list를 받아 화면에 반영
- */
 public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder> {
 
-    // 갤러리 화면에에 보여줄 이미지들의 경로 list
     private List<String> imagePaths;
 
-    // galleryActivity의 메서드 (setControlButton)를 호출하기 위한 변수
-    // 사진 선택시 toolbar의 edit/done 옵션 메뉴 버튼 활성화/비활성화
-    private GalleryActivity galleryActivity;
-    // Glide library
-    // requestManager = Glide.with(this), this = GalleryFragment
-    private RequestManager requestManager;
+    private Context context;
 
-    // selectedViewHolder : 선택된 사진의 ViewHolder (다른 사진 선택했을 때, 이전 사진 선택 효과 INVISIBLE 할 때 사용)
     private GalleryViewHolder selectedViewHolder;
-    // selectedImagePath : 선택된 사진의 절대 경로 (선택한 사진의 경로를 Uri로 변환 후 GalleryActivity에 반환하여 이전 액티비티에 돌려주기 위해 사용)
-    private String selectedImagePath;
 
-    public GalleryAdapter(GalleryActivity galleryActivity, RequestManager requestManager, List<String> imagePaths) {
-        this.galleryActivity = galleryActivity;
-        this.requestManager = requestManager;
+    public GalleryAdapter(Context context, List<String> imagePaths) {
+        this.context = context;
         this.imagePaths = imagePaths;
     }
 
@@ -53,58 +34,17 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
     @Override
     public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_gallery_image, viewGroup, false);
-        return new GalleryViewHolder(view);
+        return new GalleryViewHolder(context, view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull final GalleryViewHolder galleryViewHolder, int i) {
-        final String imagePath = imagePaths.get(i);
-        requestManager.load(imagePath)
+        Glide.with(context)
+                .load(imagePaths.get(i))
                 .into(galleryViewHolder.imageView);
         FrameLayout selectedFrame = galleryViewHolder.selectedFrame;
 
-        // 첫 실행시 사진이 선택됐을 때 사진 어두워지는 효과 설정
-        // GONE = 선택 안됐을 때
-        // VISIBLE = 선택 됐을 때
         selectedFrame.setVisibility(View.GONE);
-
-        /*
-         * setOnClickListener : 사진이 선택됐을 때, 선택 해제했을 때 설정
-         * @selectedViewHolder : 사진 선택 후 다른 사진 선택할 때 이전 사진의 선택 효과를 GONE 하기 위해 임시 저장
-         * @selectedImagePath : 선택된 사진의 경로를 uri로 변환해서 GalleryActivity에 반환하기 위해 selectedImagePath에 임시 저장
-         * @selectedFrame : 사진 선택됐을 때 사진에 겹쳐서 효과를 나타낼 frame
-         * @galleryActivity.setControlButton : 사진 선택됐을 때 toolbar의 edit/done 옵션 메뉴 버튼 활성화
-         */
-        galleryViewHolder.galleryItemConstraint.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (selectedViewHolder == null && selectedImagePath == null) {
-                    // 이전에 사진 선택된 적이 없을 때, 폴더명이 바뀌어서 선택된 사진이 없을 때
-
-                    selectedViewHolder = galleryViewHolder;
-                    selectedImagePath = imagePath;
-                    galleryViewHolder.selectedFrame.setVisibility(View.VISIBLE);
-                    galleryActivity.setControlButton(true);
-
-                } else if (selectedImagePath.equals(imagePath)) {
-                    // 선택됐던 사진을 선택해제할 때
-
-                    galleryViewHolder.selectedFrame.setVisibility(View.GONE);
-                    selectedViewHolder = null;
-                    selectedImagePath = null;
-                    galleryActivity.setControlButton(false);
-
-                } else {
-                    // 선택됐던 사진이 아닌 다른 사진을 선택할 때
-
-                    selectedViewHolder.selectedFrame.setVisibility(View.GONE);
-                    selectedViewHolder = galleryViewHolder;
-                    selectedImagePath = imagePath;
-                    galleryViewHolder.selectedFrame.setVisibility(View.VISIBLE);
-                    galleryActivity.setControlButton(true);
-                }
-            }
-        });
     }
 
     @Override
@@ -112,30 +52,56 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.GalleryV
         return imagePaths.size();
     }
 
-    // 갤러리 Fragment가 처음 시작됐을 때 또는 폴더명이 변경됐을 때 image path list를 갱신하기 위해 선언
     public void setImages(List<String> images) {
         this.imagePaths = images;
     }
 
-    // 경로를 uri로 변환해서 GalleryActivity에 반환하기 위해 선언
     public Uri getSelectedImageUri() {
-        if (selectedImagePath != null) {
-            return Uri.fromFile(new File(selectedImagePath));
+        if (selectedViewHolder != null) {
+            return Uri.fromFile(new File(imagePaths.get(selectedViewHolder.getAdapterPosition())));
         }
         return null;
     }
 
+    public GalleryViewHolder getSelectedViewHolder() {
+        return selectedViewHolder;
+    }
+
+    public void setSelectedViewHolder(GalleryViewHolder selectedViewHolder) {
+        this.selectedViewHolder = selectedViewHolder;
+    }
+
     public class GalleryViewHolder extends RecyclerView.ViewHolder {
 
-        private ConstraintLayout galleryItemConstraint;
         private ImageView imageView;
         private FrameLayout selectedFrame;
 
-        public GalleryViewHolder(@NonNull View itemView) {
+        public GalleryViewHolder(final Context context, @NonNull View itemView) {
             super(itemView);
-            galleryItemConstraint = itemView.findViewById(R.id.constraint_gallery_item);
             imageView = itemView.findViewById(R.id.image_view);
             selectedFrame = itemView.findViewById(R.id.frame_selected);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (context instanceof GalleryActivity) {
+                        if (getSelectedViewHolder() == null) {
+                            setSelectedViewHolder(GalleryViewHolder.this);
+                            selectedFrame.setVisibility(View.VISIBLE);
+                            ((GalleryActivity) context).setControlButton(true);
+                        } else if (getSelectedViewHolder() == GalleryViewHolder.this) {
+                            selectedFrame.setVisibility(View.GONE);
+                            setSelectedViewHolder(null);
+                            ((GalleryActivity) context).setControlButton(false);
+                        } else {
+                            getSelectedViewHolder().selectedFrame.setVisibility(View.GONE);
+                            setSelectedViewHolder(GalleryViewHolder.this);
+                            selectedFrame.setVisibility(View.VISIBLE);
+                            ((GalleryActivity) context).setControlButton(true);
+                        }
+                    }
+                }
+            });
         }
     }
 }
